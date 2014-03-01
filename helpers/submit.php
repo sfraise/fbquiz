cdc7ab00f28f76850d61e53c0b63a928d9c6486e
sfraise <sfraise77@gmail.com>
2014-02-28 22:13:07 -0600

    Added answer check to the submit file.

--- helpers/submit.php
+++ helpers/submit.php
@@ -14,5 +14,11 @@ $userid = Input::get('userid');
     Thank you <?php echo $name; ?>!<br /><br />
     Your email is <?php echo $email; ?>.<br /><br />
     Your user id is <?php echo $userid; ?><br /><br />
-    Your answer is "<?php echo $answer; ?>".
+    Your answer is "<?php echo $answer; ?>".<br /><br />
+    <?php if($answer == 'I live in Wisconsin cold') { ?>
+        Your answer is correct, it is "I live in Wisconsin cold" today!
+    <?php } else { ?>
+        Unfortunately your answer was incorrect.<br />
+        The correct answer is "I live in Wisconsin cold".
+    <?php } ?>
 </div>
